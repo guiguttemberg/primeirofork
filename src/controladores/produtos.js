728a0f1a7ac715c0a0252a612d51901354db5217
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-07-22 15:09:12 -0300

    criado o filtro de categoria

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,7 +1,16 @@
 const produtos = require('../../dados');
 
 const listarProdutos = (req, res) => {
-    return res.json({ produtos });
+    const { category } = req.query;
+
+    const allProducts = [];
+
+    if (category) {
+        const filteredProducts = produtos.filter(produto => produto.category === category);
+        allProducts.push(...filteredProducts);
+    }
+
+    return res.json({ allProducts });
 }
 
 module.exports = { listarProdutos };
